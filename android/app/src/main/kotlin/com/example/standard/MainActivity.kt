af40f091325c729cd368d56c72159e0b3006bbc6
potato0520 <potato20040520@gmail.com>
2022-01-17 21:35:40 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/standard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.standard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/styles/style_appbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Widget styleAppBar(
+  BuildContext context, {
+  styleAppBarIcon = '',
+  styleAppBarText = '',
+  styeAppBarTitle = '',
+}) {
+  return AppBar();
+}
